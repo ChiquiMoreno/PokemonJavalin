f345ed0bdeb352f81a25146779f844382b28b750
ChiquiMoreno <mechemorenomorales@gmail.com>
2026-03-10 21:46:15 +0100

    Actualizar siguiente ronda para asignar ganadores y recargar partidas

--- src/main/java/edu/masanz/da/cw/Main.java
+++ src/main/java/edu/masanz/da/cw/Main.java
@@ -60,6 +60,7 @@ public class Main {
         app.get("/logueado/sin-partida", LigaController::mostrarSinPartida);
         //app.get("/logueado/partidas", LigaController::mostrarPartida);
 
+        app.get("/logueado/partidas/{idLiga}", LigaController::mostrarPartidas);
         app.post("/logueado/partidas/{idLiga}", LigaController::mostrarPartidas);
         app.post("/logueado/partidas/{idLiga}/resultado", LigaController::guardarResultadoMesa);
         app.post("/logueado/partidas/{idLiga}/siguiente", LigaController::siguienteRonda);
